b4e5551c637e327ca0e481dae1d18dc453e8d8d1
Toier <116945075+Toier@users.noreply.github.com>
2022-11-26 08:10:24 +0000

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# V2Ray new configuration
+# Run V2ray
+# Write V2Ray configuration
+UUID=d6ef1b68-90a0-4c34-9c06-d374a274b660
+cat << EOF > /etc/v2ray/config.json
+{
+    "inbounds": [{
+        "port": 8080,
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "$UUID",
+                "alterId": 0
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path":  "/$UUID-vmess"
+            }
+        }
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
+
+#Run V2ray
+
+/usr/bin/v2ray/v2ray  run -config=/etc/v2ray/config.json
